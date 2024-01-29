be1ea4b276b1e3348ce3a85acc4795e1a3bb2eed
Manoel <manoelpiovesan@gmail.com>
2024-01-29 00:43:28 -0300

    Adding contacts.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+const INITIAL_PAGE = 0;
+
 function changeFoldersView(id){
     let pageBodys = document.getElementsByClassName('page-body');
     for(let i = 0; i < pageBodys.length; i++){
         pageBodys[i].style.display = 'none';
     }
 
-    pageBodys[id].style.display = 'block';
+    pageBodys[id].style.display = 'flex';
 
     let topicItems = document.getElementsByClassName('topic-item');
     for(let i = 0; i < topicItems.length; i++){ 
@@ -15,9 +17,20 @@ function changeFoldersView(id){
 
     document.querySelector("#page-header-title").textContent = topicItems[id].textContent;
     document.querySelector("#path-title").textContent = topicItems[id].textContent;
-    
-    
-
+    lastPage = id;
 }
 
-changeFoldersView(0);
+changeFoldersView(INITIAL_PAGE);
+
+function closeWindow(){
+    document.querySelector("#window").style.display = 'none';
+}
+
+///
+
+
+
+
+
+
+
